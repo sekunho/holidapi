ebbc3fb4a607fee9a9caf2710d2879602dddc2af
SEKUN <sekun@hey.com>
2022-05-02 19:38:41 +0800

    feat: Add `AddCustomHoliday`
    This represents the request to add a custom holiday. It does basic
    validations like checking if the year is not negative, or if the
    range follows a certain property. But it currently does not validate if
    the day of a certain month is valid.
    
    Note: This is still a draft.

---
+++ lib/holidefs_api/request/add_custom_holiday.ex
@@ -0,0 +1,9 @@
+defmodule HolidefsApi.Request.AddCustomHoliday do
+  @moduledoc """
+  For when the user wants to add a custom holiday.
+
+  It isn't that important to make a distinction between an informal and formal
+  holiday, at least enough to use tagged tuples. That information can just exist
+  in the struct's field instead.
+  """
+end
---
+++ test/holidefs_api/request/add_custom_holiday_test.exs
@@ -0,0 +1,7 @@
+defmodule HolidefsApi.Request.AddCustomHolidayTest do
+  use ExUnit.Case, async: true
+  import HolidefsApi.Request.AddCustomHoliday
+  alias HolidefsApi.Request.AddCustomHoliday
+
+  doctest HolidefsApi.Request.AddCustomHoliday
+end
