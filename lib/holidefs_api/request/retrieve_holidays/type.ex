a394265432ac388066abb68e1bc1ad68c9df6dc8
SEKUN <sekun@hey.com>
2022-05-02 16:02:48 +0800

    feat: Add model for `RetrieveHolidays`
    `Request.RetrieveHolidays` is a struct that I use to encode an action
    in. One can say it's not really necessary, since I could just use a map,
    and to a certain extent, that is true. But I want to be able to narrow
    down things even further so that it removes any wiggle room for
    mistakes.
    
    So, ideally, any function that needs to handle the `RetrieveHolidays`
    request should just set it as its input in the type specs, or even
    pattern match against it, and you'll be given the guarantee that
    all fields are present.
    
    Not exactly as good as languages with algebraic data types, but good
    enough, I suppose.

---
+++ lib/holidefs_api/request/retrieve_holidays/type.ex
@@ -0,0 +1,65 @@
+defmodule HolidefsApi.Request.RetrieveHolidays.Type do
+
+  @moduledoc """
+  `RetrieveHolidays.Type` is for determining if this kind of request wants
+  informal holidays to be included in the response, or if it just wants the
+  formal holidays.
+
+  This is ideally used in conjunction with `RetrieveHolidays`, specifically
+  its `type` field.
+  """
+
+  use TypedStruct
+
+  @typedoc """
+  A `type` can either be informal or formal. This tags `Type.t()` with more
+  information that people will be able to determine what type of request this
+  is.
+  """
+  @type type :: {:formal, t()} | {:include_informal, t()}
+
+  typedstruct do
+    @typedoc "Used for encoding details of the `RetrieveHolidays` request."
+    field :countries, [Holidefs.locale_code()], enforce: true
+    field :from, Date.t(), enforce: true
+    field :to, Date.t(), enforce: true
+  end
+
+  @doc """
+  Converts a map to a type of `RetrieveHolidays` request.
+
+  ## Examples
+
+      iex> from([:ph], ~D[2022-01-01], ~D[2022-04-03], "formal")
+      {:ok, {:formal, %Type{countries: [:ph], from: ~D[2022-01-01], to: ~D[2022-04-03]}}}
+
+      iex> from([:ph], ~D[2022-01-01], ~D[2022-04-03], "include_informal")
+      {:ok, {:include_informal, %Type{countries: [:ph], from: ~D[2022-01-01], to: ~D[2022-04-03]}}}
+
+  ## Panics
+
+  `from/4` will panic in the ff scenarios:
+  """
+  @spec from(
+    [Holidefs.locale_code()],
+    Date.t(), Date.t(),
+    String.t()
+  ) :: {:ok, {:formal | :include_informal, t()}}
+  def from(countries, from_date, to_date, holiday_type) do
+    type = %__MODULE__{
+      countries: countries,
+      from: from_date,
+      to: to_date
+    }
+
+    case holiday_type do
+      "formal" ->
+        {:ok, {:formal, type}}
+
+      "include_informal" ->
+        {:ok, {:include_informal, type}}
+
+      _ -> {:error, :invalid_holiday_type}
+    end
+  end
+end
---
+++ test/holidefs_api/request/retrieve_holidays/type_test.exs
@@ -0,0 +1,7 @@
+defmodule HolidefsApi.Request.RetrieveHolidays.TypeTest do
+  use ExUnit.Case, async: true
+  import HolidefsApi.Request.RetrieveHolidays.Type
+  alias HolidefsApi.Request.RetrieveHolidays.Type
+
+  doctest HolidefsApi.Request.RetrieveHolidays.Type
+end
